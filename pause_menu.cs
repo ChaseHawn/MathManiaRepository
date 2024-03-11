785f4cf001e51b9a4f95fdf5b8ff1f4224de73c5
ChaseHawn <chawn1@ocdsb.ca>
2024-03-11 09:39:43 -0400

    Minor Changes

--- pause_menu.cs
+++ pause_menu.cs
@@ -3,17 +3,42 @@ using System;
 
 public partial class pause_menu : Control
 {
-	[Export]
 	public world1_game World1;
-	
+
 	public override void _Ready()
-	{
+	{	
 		Hide();
+		World1 = GetNode<world1_game>("res://world1.tscn");
+		World1.Connect("SomeDataReady", this, nameof(OnSomeDataReady));
 	}
 
-	public void _on_resume_button_pressed()
+	public void OnSomeDataReady(int data);
 	{
-		GetTree().Paused = false;
-		Hide();
+		GD.Print("DATA RECEIVED: " + data);
 	}
+
+	// [Export] public world1_game World1;
+
+	// public override void _Ready()
+	// {	
+	// 	Hide();
+	// 	Node2D Testvar = GetNode<Node2D>("World1");
+	// 	Testvar.Connect("ToggleGamePaused", this, nameof(Onworld1_gameToggleGamePaused));
+	// }
+
+	// public void _on_resume_button_pressed()
+	// {
+	// 	GetTree().Paused = false;
+	// 	Hide();
+	// }
+
+	// public void _on_world1_menu_toggle_game_paused(bool isPaused)
+	// {
+	// 	if (isPaused){
+	// 		Show();
+	// 	}
+	// 	else {
+	// 		Hide();
+	// 	}
+	// }
 }
--- world1_game.cs
+++ world1_game.cs
@@ -4,31 +4,41 @@ using System.Dynamic;
 
 public partial class world1_game : Node2D
 {
+	public override void _Input(InputEvent @event)
+    {
+        GD.Print("TEST!!");
+    }
+	public int SomeData;
 
-	[Signal]
-	public delegate	void ToggleGamePausedEventHandler(bool isPaused);
-	public bool gamePaused = false;
+	public override void _Ready()
+	{	
+		SomeData = 43;
+		EmitSignal("SomeDataReady", SomeData);
+	}
+	// [Signal]
+	// public delegate	void ToggleGamePausedEventHandler(bool isPaused);
+	// public bool gamePaused = false;
 	
-	public bool Test
-	{
-		get
-		{
-			return gamePaused;
-		}
-		set
-		{
-			gamePaused = false;
-			GetTree().Paused = gamePaused;
-			EmitSignal("ToggleGamePaused", gamePaused);
-		}
-	}
+	// public bool Test
+	// {
+	// 	get
+	// 	{
+	// 		return gamePaused;
+	// 	}
+	// 	set
+	// 	{
+	// 		gamePaused = false;
+	// 		GetTree().Paused = gamePaused;
+	// 		EmitSignal("ToggleGamePaused", gamePaused);
+	// 	}
+	// }
 
-	public override void _UnhandledInput(InputEvent @event)
-	{
-	if (@event is InputEventKey eventKey)
-		if (eventKey.Pressed && eventKey.Keycode == Key.Escape)
-		{
-			GetTree().Paused = !gamePaused;
-		}
-	}
+	// public override void _UnhandledInput(InputEvent @event)
+	// {
+	// if (@event is InputEventKey eventKey)
+	// 	if (eventKey.Pressed && eventKey.Keycode == Key.Escape)
+	// 	{
+	// 		Test = !gamePaused;
+	// 	}
+	// }
 }
