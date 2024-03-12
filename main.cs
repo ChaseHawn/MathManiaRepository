aab6413b69f5f8a852b6497fb3445d07b0b5d737
ChaseHawn <chawn1@ocdsb.ca>
2024-03-11 20:40:50 -0400

    Minor Changes

--- main.cs
+++ main.cs
@@ -3,13 +3,15 @@ using System;
 
 public partial class main : Control
 {
-	// Called when the node enters the scene tree for the first time.
-	public override void _Ready()
+	private void OnPlayPressed()
 	{
+		// Switches to world scene
+		GetTree().ChangeSceneToFile("res://world1.tscn");
 	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	
+	private void OnQuitPressed()
 	{
+		// Exits the program
+		GetTree().Quit();
 	}
 }
