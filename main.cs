be80ff42c6334b7435a845a4d5e3e3cc19d2fdcc
ChaseHawn <chawn1@ocdsb.ca>
2024-03-12 22:15:22 -0400

    Main menu changes and sound effects

--- main.cs
+++ main.cs
@@ -1,17 +1,39 @@
 using Godot;
 using System;
+using System.Threading.Tasks;
 
 public partial class main : Control
 {
+	private AudioStreamPlayer ClickSound;
+	public bool flag;
+	public override void _Ready()
+    {
+		ClickSound = GetNode<AudioStreamPlayer>("ClickSound");
+    }
+	
 	private void OnPlayPressed()
 	{
-		// Switches to world scene
-		GetTree().ChangeSceneToFile("res://world1.tscn");
+		// Click sound plays before switching to world1 scene 
+		ClickSound.Play();
+		flag = true;
 	}
 	
-	private void OnQuitPressed()
+	private async void OnQuitPressed()
 	{
-		// Exits the program
-		GetTree().Quit();
+		// Click sound plays and a 1 second delay to ensure it plays before quitting the game
+		ClickSound.Play();
+		flag = false;
+	}
+
+	private void OnClickSoundFinished()
+	{
+		// 
+		// Switches to world scene
+		if (flag == true){
+			GetTree().ChangeSceneToFile("res://world1.tscn");
+		}
+		else {
+			GetTree().Quit();
+		}
 	}
 }
