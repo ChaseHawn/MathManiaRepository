1585ecc00055362ea42f3b857eb117716c8ff72c
1314nenon <125220340+1314nenon@users.noreply.github.com>
2024-03-28 10:19:49 -0400

    oijoi

--- scenes/BattleUI.gd
+++ scenes/BattleUI.gd
@@ -1,11 +1,25 @@
 extends Control
 
+signal textbox_closed
 
+@export var enemy: Resource
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Textbox1.hide()
-	display_text("The battle starts")
+	set_health($Playercontainer/PlayerHP,State.current_health, State.max_health)
+	set_health($Enemy1container/Enemy1HP,enemy.health,enemy.health)
 	
+	$Textbox1.hide()
+	$OptionsPanel.hide()
+	display_text("The battle starts")
+	await self.textbox_closed
+	%OptionsPanel.show()
+	
+func set_health(progress_bar, current_health, max_health):
+	progress_bar.value = current_health
+	progress_bar.max_value = max_health
+	progress_bar.get_node("Label").text = "HP: %d/%d" % [current_health, max_health]
+	pass
+
 func _input(event):
 	if Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT) and %Textbox1.visible:
 		%Textbox1.hide()
@@ -17,3 +31,7 @@ func display_text(text):
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _on_textbox_1_hidden():
+	emit_signal("textbox_closed")
+	pass # Replace with function body.
---
+++ scenes/EnemyProperties.gd
@@ -0,0 +1,6 @@
+extends Resource
+
+@export var name: String = "Enemy"
+@export var texture: Texture
+@export var health: int = 50
+@export var damage: int = 20
